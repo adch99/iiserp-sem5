84220e2793613c071d27252be4e26a9482ceaa73
Aditya Chincholi <adityachincholi@gmail.com>
2020-08-30 22:30:35 +0530

    Changed to dict notation. Backend first draft completed.

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pandas import read_csv
+import os
 
 # Filenames
-courseTemplateFilename = ""
-indexTemplateFilename = ""
+courseTemplateFilename = "course_template.html"
+indexTemplateFilename = "index_template.html"
 dataFilename = "data.csv"
+instructorDataFilename = "instructors.csv"
+# addResourcesFilename = "addresources.csv"
 indexFilename = "index.html"
 
 # Import data
-data = read_csv(dataFilename, delimiter=",", header=0, index_col="id")
+courses = read_csv(dataFilename, delimiter=",", header=0, index_col="id")
+courses = courses.to_dict("records")
+instData = read_csv(instructorDataFilename, delimiter=",", header=0, index_col="iiserid")
+instData = instData.to_dict("records")
+
+# Merge the datasets
+for course in courses:
+    instructors = []
+    for prof in instData:
+        if prof["courseid"] == course["id"]:
+            instructor.append(prof)
+    course["instructors"] = instructors
 
 # Jinja Setup
-env = Environment(loader=FileSystemLoader, autoescape=select_autoescape(["html"]))
+env = Environment(loader=FileSystemLoader(os.path.abspath(".")), autoescape=select_autoescape(["html"]))
 courseTemplate = env.get_template(courseTemplateFilename)
-indexTemplateFilename = env.get_template(indexTemplateFilename)
-
-# Classes for instructor and course
-
-class Course():
-    pass
+indexTemplate = env.get_template(indexTemplateFilename)
 
 # Start working
+for course in courses:
+    course["courseinfo"] = courseTemplate.render(course)
 
+output = indexTemplate.render(courses)
 
+with open(indexFilename, "w") as outfile:
+    outfile.write(output)
 
+print("Tasked Completed Successfully!")
