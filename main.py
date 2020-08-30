9b71a13121964a962ca9c6e33db2555457b5a77d
Aditya Chincholi <adityachincholi@gmail.com>
2020-08-30 22:58:36 +0530

    Frontend fixed, model working

--- main.py
+++ main.py
@@ -11,17 +11,21 @@ instructorDataFilename = "instructors.csv"
 indexFilename = "index.html"
 
 # Import data
-courses = read_csv(dataFilename, delimiter=",", header=0, index_col="id")
+courses = read_csv(dataFilename, delimiter=",", header=0)
 courses = courses.to_dict("records")
-instData = read_csv(instructorDataFilename, delimiter=",", header=0, index_col="iiserid")
+instData = read_csv(instructorDataFilename, delimiter=",", header=0)
 instData = instData.to_dict("records")
 
+print("courses:", courses)
+print()
+print("instData:", instData)
+print()
 # Merge the datasets
 for course in courses:
     instructors = []
     for prof in instData:
         if prof["courseid"] == course["id"]:
-            instructor.append(prof)
+            instructors.append(prof)
     course["instructors"] = instructors
 
 # Jinja Setup
@@ -31,9 +35,12 @@ indexTemplate = env.get_template(indexTemplateFilename)
 
 # Start working
 for course in courses:
-    course["courseinfo"] = courseTemplate.render(course)
+    print("course:", course)
+    print()
+    course["coursehtml"] = courseTemplate.render(course=course)
+    # print("courseinfo:", course["courseinfo"])
 
-output = indexTemplate.render(courses)
+output = indexTemplate.render(courses=courses)
 
 with open(indexFilename, "w") as outfile:
     outfile.write(output)
