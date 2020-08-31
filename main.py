c23adc81991bff1828a0da52cd00e4f1e1daa0c9
Aditya Chincholi <adityachincholi@gmail.com>
2020-08-31 20:06:06 +0530

    Fixed an error in the data

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import os
 # Filenames
 courseTemplateFilename = "templates/course_template.html"
 deptTemplateFilename = "templates/dept_template.html"
-dataFilename = "data/hss_courses.csv"
-instructorDataFilename = "data/hss_instructors.csv"
+dataFilename = "data/mth_courses.csv"
+instructorDataFilename = "data/mth_instructors.csv"
 # addResourcesFilename = "addresources.csv"
-deptFilename = "html/hss.html"
+deptFilename = "html/mth.html"
 
 # Import data
 
